11d05c4c9242bf53d1be0604888c1730a014ddb9
IoPhoenix <olga.fomin26@gmail.com>
2018-05-07 21:13:21 -0700

    Added navigation for profile route

--- controllers/updateProfile.js
+++ controllers/updateProfile.js
@@ -1,17 +1,16 @@
 const updateProfile = (req, res, db) => {
-    const { id, newName, newEmail } = req.body;
+    const { id, newName } = req.body;
 
     db('users')
         .where('id', '=', id)
         .update({name: newName})
-        .update({email: newEmail})
         .returning('name')
         .then(data => {
           res.json(data)
         })
         .catch(err => {
             console.log(err);
-            res.status(400).json('Error updating user details');
+            res.status(400).json('Error updating user name');
         });
 }
 
