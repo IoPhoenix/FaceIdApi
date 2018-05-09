09da8a2a17e10e06b357109724fa6e0d14ee0e77
IoPhoenix <olga.fomin26@gmail.com>
2018-05-08 23:36:33 -0700

    Experimenting with email update in database

--- controllers/updateProfile.js
+++ controllers/updateProfile.js
@@ -1,16 +1,18 @@
 const updateProfile = (req, res, db) => {
-    const { id, newName } = req.body;
+    const { id, newName, newEmail } = req.body;
+    console.log(id, newName, newEmail);
 
     db('users')
         .where('id', '=', id)
         .update({name: newName})
-        .returning('name')
+        .update({email: newEmail})
+        .returning('email')
         .then(data => {
           res.json(data)
         })
         .catch(err => {
             console.log(err);
-            res.status(400).json('Error updating user name');
+            res.status(400).json(err);
         });
 }
 
