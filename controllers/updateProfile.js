c4a892bdde625efac65538fd3a2ef18bb5227fd2
IoPhoenix <olga.fomin26@gmail.com>
2018-05-09 00:40:14 -0700

    Experimenting with email update in database

--- controllers/updateProfile.js
+++ controllers/updateProfile.js
@@ -8,20 +8,31 @@ const updateProfile = (req, res, db) => {
         .update({email: newEmail})
         .then(data => {
             db('login')
-                .where('id', '=', id)
-                .update({email: newEmail})
-                .returning('email')
+                .select()
+                .where('email', '=', newEmail)
+                .then((rows) => {
+                    if (rows.length === 0) {
+                        // no matching email found
+                        return db('login').where('id', '=', id).update({email: newEmail}).returning('email')
+                    } else {
+                        // return or throw - duplicate name found
+                        return 'Duplicate email found'
+                    }
+                })
+                // .where('id', '=', id)
+                // .update({email: newEmail})
+                // .returning('email')
                 .then(data => {
                     res.json(data)
                 })
                 .catch(err => {
                     console.log(err);
-                    res.status(400).json(err);
+                    res.status(400).json('Unable to update your email');
                 });
         })
         .catch(err => {
             console.log(err);
-            res.status(400).json(err);
+            res.status(400).json('Unable to update your details');
         });
 }
 
