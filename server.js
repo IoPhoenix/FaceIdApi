f5d0a41b31530514152e18ce581c665138b4544b
IoPhoenix <olga.fomin26@gmail.com>
2018-10-25 12:36:23 -0700

    Updated scripts in package.json

--- server.js
+++ server.js
@@ -94,7 +94,7 @@ app.put('/avatar', (req, res) => { avatar.updateAvatar(req, res, db)});
 
 app.listen(process.env.PORT || 3000, () => {
     console.log(`App is running on ${process.env.PORT}`);
-});
+})
 
 // app.listen(3000, ()=> {
 //     console.log('app is running on port 3000');
